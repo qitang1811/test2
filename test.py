0989c774dad0e2378a3f5d3963ecdaaa8a2980a7
qitang1811 <qitang31@gmail.com>
2013-08-07 11:20:27 -0400

    change files

---
+++ test.py
@@ -0,0 +1,27 @@
+def pow(num, e):
+	"""
+	calculate the pow of num
+	"""
+
+	if e < 0:
+		return -1
+	base = 1
+	for i in range(e):
+		base *= num
+	return base
+
+def add(a ,b):
+	return a+b
+
+def minus(a,b):
+	return a-b
+
+def main():
+	num = 2
+	e = 2
+	print pow(num, e)
+	print add(num ,e)
+	print minus(3,4)
+
+if __name__=="__main__":
+	main()
